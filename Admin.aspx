42ef15c34569cc78833fbff33e32e50f47b1a152
NagaSwetha2 <knagaswetha2498@siu.edu>
2017-02-24 21:19:03 -0600

    Admin login
    Admin's can login using thier user name and password to access user
    details

---
+++ Admin.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Admin.aspx.cs" Inherits="_Default" %>
+
+
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>WELCOME ADMIN!</title>
+    <link href="css/Style.css" rel="stylesheet" type="text/css" />
+    
+</head>
+<body style="background-color:#eee;color:#333333">
+  <hr />  <p style="text-align:left;">Timesheet application for Computer Science Department</p><hr />
+    <form id="form1" runat="server">
+    <div >
+    <div >
+        <table style=" width : 70%; height: 50px;">
+            <tr style="padding:25px">
+                    <td >username:
+                    </td>
+                     <td >
+                    <asp:TextBox ID="TextBox1" runat="server" ></asp:TextBox>
+                         <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Enter valid user name" ControlToValidate="TextBox1"  Width="114px"></asp:RequiredFieldValidator>       
+                  </td>
+                    </tr>
+            <tr  >
+                <td >password:
+                </td>
+                <td >
+               <asp:TextBox ID="TextBox2" runat="server" TextMode="Password"></asp:TextBox>
+             <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Enter valid password" ControlToValidate="TextBox2" Width="114px"></asp:RequiredFieldValidator>       
+             </td>  
+            </tr>
+          
+           
+        </table>
+       
+      </div>  </div>
+    
+    <asp:Button ID="Button1" runat="server" Text="LOGIN"    OnClick="Button1_Click" CausesValidation="true"  Width="114px" Height="20px"/>
+   
+  
+   <asp:Button ID="Button2" runat="server" Text="BACK"  OnClick="Button2_Click" CausesValidation="false" Width="114px" Height="20px"/>
+    
+   
+        
+    
+      
+
+    </form>
+</body>
+</html>
---
+++ Admin.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=SWETHA;Initial Catalog=master;Integrated Security=True");
+    
+  
+    //SqlConnection con = new SqlConnection(@"Data Source=LIB-LAXMAN\SQLEXPRESS;Initial Catalog=timesheet_project;User ID=sa;Password=qwertypoiu123!");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        string query = "select count(*) from admins where username='" + TextBox1.Text + "'";
+        SqlCommand cmd = new SqlCommand(query, con);
+        int temp = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+        con.Close();
+        if (temp == 1)
+        {
+            con.Open();
+            string pass = "select password from admins where username='" + TextBox1.Text + "'";
+            SqlCommand passcmd = new SqlCommand(pass, con);
+            string password = passcmd.ExecuteScalar().ToString();
+            if (password == TextBox2.Text)
+            {
+                Session["admin"] = TextBox1.Text;
+
+                //  Response.Redirect("C:/Users/Systems/Documents/Visual Studio 2012/WebSites/sheet/admin_functions.aspx");
+
+                Server.Transfer("admin_functions.aspx");
+
+                /*string q = "insert into info(u_id) values('" + TextBox1.Text + "')";
+                 SqlCommand cm = new SqlCommand(q, con);
+                 cm.ExecuteNonQuery();*/
+
+
+
+            }
+            else
+            {
+                Response.Write("password is incorrect");
+            }
+        }
+        else
+        {
+            Response.Write("dawg is not correct");
+        }
+        con.Close();
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+       // Response.Redirect("./");
+        Server.Transfer("home.aspx");
+
+
+    }
+}
