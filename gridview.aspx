82e309b08b7381a2f0a639b0c545ad6625ecb040
NagaSwetha2 <knagaswetha2498@siu.edu>
2017-02-24 21:34:26 -0600

    user- login and logout
    The user can login and logout using a  single click without entering
    date,time. This page also supports unique features: It allows users to
    see all other user's login details of the day.But it restricts current
    user from modifying other user's data

---
+++ gridview.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="gridview.aspx.cs" Inherits="gridview" %>
+
+
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+     <link href="css/Style.css" rel="stylesheet" type="text/css" />
+
+    </head>
+<body  style="background-color:#eee;color:#333333">
+   <hr /> <p style="text-align:left;">Timesheet application for Computer Science Department</p><hr />
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="clock_in" DataSourceID="SqlDataSource5" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" AllowPaging="True">
+            <Columns>
+                <asp:BoundField DataField="dawg" HeaderText="dawg" SortExpression="dawg" />
+                <asp:BoundField DataField="name" HeaderText="name" SortExpression="name" />
+                <asp:BoundField DataField="clock_in" HeaderText="clock_in" ReadOnly="True" SortExpression="clock_in" />
+                <asp:BoundField DataField="clock_out" HeaderText="clock_out" SortExpression="clock_out" />
+                <asp:BoundField DataField="hrs_paid" HeaderText="hrs_paid" SortExpression="hrs_paid" />
+               
+            </Columns>
+        </asp:GridView>
+
+        
+        <asp:SqlDataSource ID="SqlDataSource5" runat="server" ConnectionString="<%$ ConnectionStrings:masterConnectionString %>" SelectCommand="SELECT c.dawg, b.name, c.clock_in, c.clock_out, c.hrs_paid, c.lunch FROM info AS c INNER JOIN time_sheets AS b ON c.dawg = b.id WHERE (CONVERT (date, c.created_date) = CONVERT (date, GETDATE()))" > </asp:SqlDataSource>
+
+        
+        <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:masterConnectionString %>" SelectCommand="SELECT [dawg], [clock_in], [clock_out], [hrs_paid] FROM [info]"></asp:SqlDataSource>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:masterConnectionString %>" SelectCommand="SELECT [dawg], [clock_in], [clock_out], [hrs_paid] FROM [info]"></asp:SqlDataSource>
+        <asp:Button ID="Button1" runat="server"  OnClick="Button1_Click" Text="CLOCK IN" />
+        <asp:Button ID="Button2" runat="server"   OnClick="Button2_Click" Text="CLOCK OUT" />
+        <asp:TextBox ID="TextBox1" runat="server"  ReadOnly="true" ></asp:TextBox>
+        <br />
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        &nbsp;</td>
+                    
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        <asp:LinkButton ID="LinkButton1" runat="server"     OnClick="LinkButton1_Click"  CausesValidation="false">LOGOUT</asp:LinkButton>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+            </table>
+        </div>
+
+
+
+        <p>
+           
+
+           
+            
+            &nbsp;</p>
+        <p>
+           
+
+           
+            
+            &nbsp;</p>
+        <p>
+           
+
+           
+            
+            &nbsp;</p>
+        
+    </form>
+</body>
+</html>
+
---
+++ gridview.aspx.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class gridview : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=SWETHA;Initial Catalog=master;Integrated Security=True");
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+            name();
+            display();
+            bindgrid();
+            visibility();
+            GridView1.DataBind();
+        }
+    }
+
+    public void name()
+    {
+        SqlCommand cmd = new SqlCommand("update info set name=(select name from time_sheets where id=@id) where dawg=@d and  CONVERT(date,created_date) = CONVERT(date, GETDATE())", con);
+        cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+        cmd.Parameters.AddWithValue("@d", TextBox1.Text);
+        con.Close();
+        GridView1.DataBind();
+    }
+
+    public void display()
+    {
+        con.Open();
+        string s = "select * from info where CONVERT(date,created_date) = CONVERT(date, GETDATE())";
+        SqlCommand cmd = new SqlCommand(s, con);
+        con.Close();
+        visibility();
+    }
+
+    public void bindgrid()
+    {
+        if (Session["New"] != null)
+        {
+            TextBox1.Text = Session["New"].ToString();
+        }
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        Server.Transfer("home.aspx");
+        // Response.Redirect("./");
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        string s = "insert into info(dawg,clock_in,created_date) values(@u,@il,@l)";
+        SqlCommand cmd = new SqlCommand(s, con);
+        cmd.Parameters.AddWithValue("@u", TextBox1.Text);
+        //DateTime.Now.ToString("mm/dd/yyyy HH:mm:ss");
+        cmd.Parameters.AddWithValue("@il", DateTime.Now.ToString());
+        cmd.Parameters.AddWithValue("@l", DateTime.Today);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        pay();
+        pay_end();
+        name();
+        bindgrid();
+        visibility();
+        GridView1.DataBind();
+
+    }
+
+    void pay()
+    {
+        con.Open();
+        string s = "update info set pay=(select dateadd(dd,14, (select min(created_date) from info)))";
+        SqlCommand cmd = new SqlCommand(s, con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+    }
+
+    void pay_end()
+    {
+        con.Open();
+        string o = "update info set end_pay=(select dateadd(day,1,min(pay)) from info)";
+
+        SqlCommand cmd = new SqlCommand(o, con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        con.Close();
+
+    }
+
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("update info set clock_out = @cout where dawg = @id and created_date = @cd and clock_out is null", con);
+        cmd.Parameters.AddWithValue("@cout", DateTime.Now.ToString());
+        // cmd.Parameters.AddWithValue("@hrs", (DATEDIFF(minute, clock_in, clock_out)) where dawg = '"++"'));
+        cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+        cmd.Parameters.AddWithValue("@cd", DateTime.Today);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        hours();
+        GridView1.DataBind();
+        unpaid();
+        GridView1.DataBind();
+        total();
+        GridView1.DataBind();
+        lunch();
+        GridView1.DataBind();
+        visibility();
+        GridView1.DataBind();
+
+    }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        con.Open();
+        string s = "select dawg,clock_in,clock_out,hrs_paid from info where dawg='" + TextBox1.Text + "' and CONVERT(date, created_date) = CONVERT(date, GETDATE()) ";
+        SqlCommand cmd = new SqlCommand(s, con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        visibility();
+        DataBind();
+        bindgrid();
+    }
+
+    public void hours()
+    {
+        con.Open();
+        string s = "update info set hrs_paid=(DATEDIFF(MINUTE,clock_in,clock_out)) /60.0 where dawg='" + TextBox1.Text + "'"; //by 60.0 is there
+        SqlCommand sql = new SqlCommand(s, con);
+        sql.ExecuteNonQuery();
+        con.Close();
+    }
+
+    public void unpaid()
+    {
+        con.Open();
+        string query = "  select COUNT(*) from info  where dawg='" + TextBox1.Text + "' and CONVERT(date,created_date) = CONVERT(date, GETDATE())";
+        SqlCommand cmd = new SqlCommand(query, con);
+        int temp = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+        con.Close();
+        if (temp > 2)
+        {
+            con.Open();
+        string s = " UPDATE INFO SET unpaid_hrs=(SELECT(ROUND((SELECT(DATEDIFF(MINUTE,( SELECT MIN(clock_in) FROM INFO WHERE dawg='" + TextBox1.Text + "' and CONVERT(date,created_date)=CONVERT(date,GETDATE())),  (SELECT MAX(clock_out) FROM INFO WHERE (dawg='" + TextBox1.Text + "') and CONVERT(date,created_date)=CONVERT(date,GETDATE()))))/60.0),3))) FROM INFO WHERE DAWG='" + TextBox1.Text + "' AND CONVERT(DATE,created_date)=CONVERT(date,getdate())";
+        SqlCommand sql = new SqlCommand(s, con);
+        sql.ExecuteNonQuery();
+        con.Close();
+    }
+}
+    public void total()
+    {
+        con.Open();
+        string s = " update info set total=(select round((select sum(hrs_paid) from info where dawg='" + TextBox1.Text + "' and (convert(date,created_date)=convert(date,getdate()))),2)) where dawg='" + TextBox1.Text + "' and (convert(date,created_date)=CONVERT(date,getdate()))";
+        SqlCommand sql = new SqlCommand(s, con);
+        sql.ExecuteNonQuery();
+        con.Close();
+    }
+
+    public void lunch()
+    {
+        con.Open();
+        string query = "  select COUNT(*) from info  where dawg='" + TextBox1.Text + "' and CONVERT(date,created_date) = CONVERT(date, GETDATE())";
+        SqlCommand cmd = new SqlCommand(query, con);
+        int temp = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+        con.Close();
+        if (temp >1 )
+        {
+            con.Open();
+            string s = "update info set lunch = (select (unpaid_hrs-total)) where dawg = '" + TextBox1.Text + "' and (CONVERT(date, created_date) = CONVERT(date, GETDATE()))";
+            SqlCommand sql = new SqlCommand(s, con);
+            sql.ExecuteNonQuery();
+            con.Close();
+        }
+        
+    }
+    public void visibility()
+    {
+        bindgrid();
+        con.Open();
+        string s = "select * from info where clock_out is null and  (CONVERT(date, created_date)) = (CONVERT(date, GETDATE())) and dawg = '" + TextBox1.Text + "'";
+        using (SqlCommand command = new SqlCommand(s, con))
+        {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                // if the result set is not NULL
+                if (reader.HasRows)
+                {
+                    Button1.Visible = false;
+                    Button2.Visible = true;
+
+                }
+                else
+                {
+                    Button1.Visible = true;
+                    Button2.Visible = false;
+                }
+            }
+        }
+        con.Close();
+    }
+
+    public void datecol()
+    {
+        con.Open();
+        string s1 = "update info set datecol= (select convert(date,getdate()) from info where dawg='" + TextBox1.Text + "') where dawg='" + TextBox1.Text + "'";
+        SqlCommand sql1 = new SqlCommand(s1, con);
+        sql1.ExecuteNonQuery();
+        con.Close();
+    }
+}
